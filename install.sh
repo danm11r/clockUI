c3c3206c3ddf7140842a8dd81b0423bed917a5ff
danmi11r <dmiller-dev@proton.me>
2024-09-01 15:04:54 -0600

    Updated install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install necessary packages
-sudo apt-get install python3 python3.pyqt5 python3-pyqt5.qtquick qml-module-qtquick-shapes qml-module-qtquick-controls2 python3-dotenv qml-module-qt-labs-settings -y
+sudo apt-get install python3 python3.pyqt5 python3-pyqt5.qtquick qml-module-qtquick-shapes qml-module-qtquick-controls2 qml-module-qtquick-window2 python3-dotenv qml-module-qt-labs-settings -y
 
 # Install font
 sudo apt-get install fonts-noto -y
