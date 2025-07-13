4c799c36472d5ff6fc82fffe6131850d638c68a5
danm11r <dmiller-dev@proton.me>
2025-07-13 00:00:52 -0600

    Quick fix to allow start script to work with crontab

--- start.sh
+++ start.sh
@@ -2,8 +2,12 @@
 
 # Start script for clockUI
 
+# crontab will execute this script from the user's home directory, 
+# so it is necessary to change into the clockUI directory
+
 {
 echo Starting clockUI at $(date)
+cd clockUI
 source venv/bin/activate
 python3 main.py --platform eglfs
 } &>> log.txt
